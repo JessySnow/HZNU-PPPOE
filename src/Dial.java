26cf46e06c526bf44a46c3573b115f49f5e74626
Jessy Snow <jessysnow12345@gmail.com>
2021-05-07 20:54:41 +0800

    Little improve.

--- src/Dial.java
+++ src/Dial.java
@@ -11,6 +11,12 @@ import java.io.*;
 public class Dial {
     public static void main(String[] args) throws Exception{
         Scanner keyIn = new Scanner(System.in);
-
+        String Command = "rasdial PPPOE ";
+        String UserName, PassWord, BLANK;
+        if(!Config.returnConfiged()){
+            Config.ShowInterface();
+            UserName = keyIn.nextLine();
+            PassWord = keyIn.nextLine();
+        }
     }
 }
--- src/config/Config.java
+++ src/config/Config.java
@@ -5,17 +5,32 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Config{
+    //Basic variable
+    private String UserName, PassWord, Configed;
+
     private final String filePath = "src/config/config.properties";
     static Properties props = new Properties();
+
+    //Set UserName
+    //Set PassWord
+    //Set Configured
 
     //Load Properties
     public static void loadProps(){;}
 
-    //Return user_name
+    //Return UserName
     public static String returnUserName(){return "";}
 
     //Return PassWord
     public static String returnPassWord(){return "";}
 
-    
+    //Return Configed and Set Configed from "false" to "true"
+    public static boolean returnConfiged(){return false;}
+
+    //Show config Interface
+    public static void ShowInterface(){
+        System.out.println("######## Init #########");
+        System.out.println("分行输入学号的密码");
+    }
+
 }
