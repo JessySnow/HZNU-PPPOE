1dd263568a59f5b576060a00e17f39828fe3b105
Jessy Snow <jessysnow12345@gmail.com>
2021-05-08 11:06:30 +0800

    Done with config

--- src/Dial.java
+++ src/Dial.java
@@ -10,13 +10,7 @@ import java.io.*;
 
 public class Dial {
     public static void main(String[] args) throws Exception{
-        Scanner keyIn = new Scanner(System.in);
-        String Command = "rasdial PPPOE ";
-        String UserName, PassWord, BLANK;
-        if(!Config.returnConfiged()){
-            Config.ShowInterface();
-            UserName = keyIn.nextLine();
-            PassWord = keyIn.nextLine();
-        }
+        String str = System.getProperty("user.dir");
+        System.out.println(str);
     }
 }
--- src/config/Config.java
+++ src/config/Config.java
@@ -6,31 +6,60 @@ import java.util.Scanner;
 
 public class Config{
     //Basic variable
-    private String UserName, PassWord, Configed;
+    private static String UserName, PassWord, Configed;
+    private static Scanner keyIn = new Scanner(System.in);
 
-    private final String filePath = "src/config/config.properties";
+    //Properties
+    private static final String filePath = "src\\config\\config.properties";
     static Properties props = new Properties();
 
-    //Set UserName
-    //Set PassWord
-    //Set Configured
 
     //Load Properties
-    public static void loadProps(){;}
-
-    //Return UserName
-    public static String returnUserName(){return "";}
-
-    //Return PassWord
-    public static String returnPassWord(){return "";}
-
-    //Return Configed and Set Configed from "false" to "true"
-    public static boolean returnConfiged(){return false;}
-
-    //Show config Interface
-    public static void ShowInterface(){
-        System.out.println("######## Init #########");
-        System.out.println("分行输入学号的密码");
+    public static void loadProps() throws Exception{
+        FileInputStream propsIn = new FileInputStream(filePath);
+        props.load(propsIn);
+        propsIn.close();
     }
 
+    //Write Properties
+    public static void writeProps() throws Exception{
+        FileOutputStream propsOut = new FileOutputStream(filePath);
+        props.store(propsOut,"Init or Change");
+    }
+
+    //Set UserName
+    public static void setUserName() {props.setProperty("UserName", UserName);}
+    //Set PassWord
+    public static void setPassWord() {props.getProperty("PassWord", PassWord);}
+    //Set Configured
+    public static void setConfiged() {props.getProperty("Configured", Configed);}
+    //Return UserName
+    public static String returnUserName(){return props.getProperty("UserName");}
+    //Return PassWord
+    public static String returnPassWord(){return props.getProperty("PassWord");}
+    //Return Configed and Set Configed from "false" to "true"
+    public static boolean returnConfiged(){
+        if(props.getProperty("Configured").equals("true"))  return true;
+        else{
+            props.setProperty("Configured", "true");
+            return false;
+        }
+    }
+
+    //Show config Interface and Init Dial
+    public static void InitDial() throws Exception{
+        System.out.println("######## Init #########");
+        System.out.println("分行输入学号的密码");
+        UserName = keyIn.nextLine();
+        PassWord =  keyIn.nextLine();
+        loadProps();
+        setPassWord();
+        setUserName();
+        writeProps();
+    }
+
+    //Test
+    public static void main(String[] args) throws Exception{
+        InitDial();
+    }
 }
