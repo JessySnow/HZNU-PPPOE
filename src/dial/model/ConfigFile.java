72673a1938d3d996179e35a6b756f85b56ad847f
Jessy Snow <jessysnow12345@gmail.com>
2021-07-08 21:34:59 +0800

    Ready to change develop model

--- src/dial/model/ConfigFile.java
+++ src/dial/model/ConfigFile.java
@@ -1,7 +1,32 @@
 package dial.model;
 
 import java.io.IOException;
+import java.util.Properties;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
 
 public class ConfigFile {
-    private String Path;
+    private final String userDir = System.getProperty("user.dir");
+    private final String filePath = userDir + "\\config.properties";
+    private Properties props = new Properties();
+
+    /**
+     * default constructor
+     * */
+    ConfigFile(){}
+
+
+    public void initProps(){
+        try{
+            FileInputStream propsIn = new FileInputStream(filePath);
+            props.load(new java.io.FileInputStream(filePath));
+            propsIn.close();
+        }catch (IOException e){
+            System.exit(1);
+        }
+    }
+
+    public Properties getProps() {
+        return props;
+    }
 }
