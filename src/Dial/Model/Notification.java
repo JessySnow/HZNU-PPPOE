25c7ecb6ccf64ed5c9fbe11b3a83115cc124424c
Jessy Snow <jessysnow12345@gmail.com>
2021-07-08 18:56:09 +0800

    Clear the repo

--- src/Dial/Model/Notification.java
+++
@@ -1,7 +0,0 @@
-package Dial.Model;
-
-public class Notification {
-    private String successNotification = "";
-    private String failNotification = "";
-    private String errorNotification = "";
-}
--- src/Dial/Model/WIN_CMD.java
+++
@@ -1,6 +0,0 @@
-package Dial.Model;
-
-public class WIN_CMD {
-    private int statusCOde;
-    private String commandLine;
-}
