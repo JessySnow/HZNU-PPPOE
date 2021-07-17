93cd4b264d97dca79873bac399b79aa11a1847eb
Jessy Snow <jessysnow12345@gmail.com>
2021-07-17 23:49:12 +0800

    Init writing pbk

--- src/dial/WritePBK.java
+++ src/dial/WritePBK.java
@@ -6,5 +6,6 @@ package dial;
  */
 
 public class WritePBK {
-
+    private static final String pbk_path = "C:\\Users\\jessy\\AppData\\Roaming\\Microsoft\\Network\\Connections\\Pbk\\rasphone.pbk";
+    public static void writing_appending(){}
 }
