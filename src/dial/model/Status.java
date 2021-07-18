f21f94d16df2bf67eafbeaaca98857dbe9da79c3
Jessy Snow <jessysnow12345@gmail.com>
2021-07-18 20:21:32 +0800

    Clean some comment

--- src/dial/model/Status.java
+++ src/dial/model/Status.java
@@ -2,18 +2,12 @@ package dial.model;
 
 public class Status {
     private String statusInfo;
-
-    /**
-     * default constructor
-     * */
     public Status(){
-        statusInfo = "发生了未知错误_-1";
+        statusInfo = "发生了未知错误_-10";
     }
-
     public void setStatusInfo(String statusInfo) {
         this.statusInfo = statusInfo;
     }
-
     public String getStatusInfo() {
         return statusInfo;
     }
