04b18a1e133f6c25091d386fa64e3520c1fbbd8a
Jessy Snow <jessysnow12345@gmail.com>
2021-07-31 15:56:30 +0800

    May delete Status in next version

--- src/dial/model/Connection.java
+++ src/dial/model/Connection.java
@@ -2,9 +2,11 @@ package dial.model;
 
 public class Connection {
     private Status status;
+    private String statusInfo;
 
     public Connection(){
         status = new Status();
+        statusInfo = "发生了未知错误_-10";
     }
 
     public void setStatus(Status status) {
